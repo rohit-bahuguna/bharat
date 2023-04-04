11bd6db1f20be095356efeb3b1a40ea5355f2570
rohit-bahuguna <rohitbahuguna.4446@gmail.com>
2023-04-05 00:31:13 +0530

    04/04/2023

--- src/components/select/ReactSelect.js
+++ src/components/select/ReactSelect.js
@@ -4,10 +4,12 @@ const RSelect = ({ options, defaultValue, getClassData, name }) => {
 	return (
 		<div className="form-group  field-focus-box-shadow">
 			<select
+				defaultValue={defaultValue}
 				onChange={getClassData}
 				className="custom-select  form-control-wrap field-padding-y-lg  w-100"
 				name={name}>
 				<option
+					disabled
 					value={defaultValue}
 					className="field-padding-y-lg field-font-weight field-font-size">
 					{defaultValue}
